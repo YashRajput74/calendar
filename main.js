3cbf1ea82d4909ad3f5f7d8a65815eb3c1bf3118
Yash Rajput <yashrajput7455@gmail.com>
2025-04-05 23:50:42 +0530

    options UI added

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-import { controller } from "./components/calendar/controller";
+import { controller as calendarController} from "./components/calendar/controller";
 
-controller();
+calendarController();
+/**
+ * i was thinking that for the optionsDiv i will make different componentfolder(model,view,controller) but it will be linked to the calender component so i will be needing to access the calendarcontroller methods, maybe i should do lke this or maybe i should embed both the functionalities in same mvc of calendar
+ */
