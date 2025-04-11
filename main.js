be264f4b577961a6f36fb71017a6c25676ca9229
Yash Rajput <yashrajput7455@gmail.com>
2025-04-11 23:08:36 +0530

    added responsiveness

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import { controller as calendarController} from "./components/calendar/controller";
-import { controller as optionsController } from "./components/optionsOfCalendar/controller";
 import { controller as weatherController } from "./components/weather/controller";
 
 calendarController.init();
 calendarController.todayDateRender();
-optionsController.init();
 weatherController.init();
